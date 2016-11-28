6fef1bb38523deac995d43f8f09e86ae2c716873
Gene Lee <Gene@GMBA.local>
2016-11-28 11:04:08 -0800

    initial HW13 code

---
+++ HW13/HW13/ConcatStream.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace CS422
+{
+    public class ConcatStream : Stream
+    {
+        Stream m_first, m_second;
+        long len, position;
+        bool isFixedLengthConstructor;
+
+        public override bool CanRead { get { return m_first.CanRead && m_second.CanRead; } }
+        public override bool CanSeek { get { return m_first.CanSeek && m_second.CanSeek; } }
+        public override bool CanWrite { get { return m_first.CanWrite && m_second.CanWrite; } }
+
+        public ConcatStream(Stream first, Stream second)
+        {
+            if (!first.CanSeek)
+                throw new NotSupportedException("First stream must support Length property");
+            /*
+             * if a class derived from Stream does not support seeking, 
+             * calls to Length, SetLength, Position, and Seek throw a NotSupportedException.
+             */
+
+            m_first = first;
+            m_second = second;
+            position = 0;
+            isFixedLengthConstructor = false;
+            try
+            {
+                len = first.Length + second.Length;
+            }
+            catch
+            {
+                len = -1;
+            }
+        }
+
+        public ConcatStream(Stream first, Stream second, long fixedLength)
+        {
+            if (fixedLength < 0)
+                throw new ArgumentException("Length cannot be less than 0");
+
+            m_first = first;
+            m_second = second;
+            position = 0;
+            len = fixedLength;
+            isFixedLengthConstructor = true;
+        }
+
+        #region implemented abstract members of Stream
+
+        public override void Flush()
+        {
+            m_first.Flush(); m_second.Flush();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            if (!CanSeek)
+                throw new NotSupportedException("Forward-only reading functionality with no seeking");
+
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    Position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    Position += offset;
+                    break;
+                case SeekOrigin.End:
+                    Position = Length + offset;
+                    break;
+            }
+            return position;
+        }
+
+        public override void SetLength(long value)
+        {
+            if (!CanSeek)
+                throw new NotSupportedException("Only forward reading is supported");
+
+            len = value < 0 ? 0 : value; // if value < 0 set to 0
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int bytesRead = 0;
+
+            if ((offset + count) > buffer.Length) // The sum of offset and count is larger than the buffer length. 
+            {
+                throw new ArgumentException();
+            }
+            else if (buffer == null) // buffer is null. 
+            {
+                throw new ArgumentNullException();
+            }
+            else if (offset < 0 || count < 0) // offset or count is negative. 
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (CanRead == false) // The stream does not support reading.  
+            {
+                throw new NotSupportedException();
+            }
+            else
+            {
+                if (Position + count <= m_first.Length) // to first stream
+                    bytesRead = m_first.Read(buffer, offset, count);
+
+                else if (Position > m_first.Length) // all to second stream
+                    bytesRead = m_second.Read(buffer, offset, count);
+
+                else // both streams
+                {
+                    int room = (int)(m_first.Length - m_first.Position);
+                    bytesRead = m_first.Read(buffer, offset, room);
+                    bytesRead += m_second.Read(buffer, offset + room, count - room);
+                }
+
+            }
+            Position += bytesRead;
+            return bytesRead;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (!CanWrite)
+                return;
+
+            if (Position + count <= m_first.Length) // to first stream
+                m_first.Write(buffer, offset, count);
+
+            else if (Position > m_first.Length) // all to second stream
+                m_second.Write(buffer, offset, count);
+
+            else // both streams
+            {
+                int room = (int)(m_first.Length - m_first.Position);
+                m_first.Write(buffer, offset, room);
+                m_second.Write(buffer, offset + room, count - room);
+            }
+            Position += count;
+        }
+
+        public override long Length
+        {
+            get
+            {
+                if (isFixedLengthConstructor)
+                    return len;
+                else
+                {
+                    if (len == -1)
+                        return m_first.Length;
+                    else
+                        return m_first.Length + m_second.Length;
+                }
+            }
+        }
+
+        public override long Position
+        {
+            get { return position; }
+            set
+            {
+                if (value < 0)
+                {
+                    position = 0;
+                    m_first.Seek(0, SeekOrigin.Begin);
+                    if (m_second.CanSeek)
+                        m_second.Seek(0, SeekOrigin.Begin);
+                }
+                else if (Length < value)
+                {
+                    position = Length;
+                    m_first.Seek(0, SeekOrigin.End);
+                    if (m_second.CanSeek)
+                        m_second.Seek(0, SeekOrigin.End);
+                }
+                else
+                {
+                    position = value;
+
+                    if (value <= m_first.Length)
+                    {
+                        m_first.Seek(value, SeekOrigin.Begin);
+                        if (m_second.CanSeek)
+                            m_second.Seek(0, SeekOrigin.Begin);
+                    }
+                    else
+                    {
+                        m_first.Seek(0, SeekOrigin.End);
+                        if (m_second.CanSeek)
+                            m_second.Seek(value - m_first.Length, SeekOrigin.Begin);
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
+
---
+++ HW13/HW13/FilesWebService.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CS422
+{
+    public class FilesWebService : WebService
+    {
+        private FileSys422 m_fs;
+        private bool m_allowUploads = true;
+
+        public FilesWebService(FileSys422 fs) 
+        {
+            m_fs = fs;
+        }
+
+        public override string ServiceURI
+        {
+            get
+            {
+                return "/files";
+            }
+        }
+
+        public override void Handler(WebRequest req)
+        {
+            if (!req.RequestURI.StartsWith(ServiceURI, StringComparison.CurrentCulture))
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (req.RequestURI == "/files" || req.RequestURI == "/files/")
+            {
+                RespondWithList(m_fs.GetRoot(), req);
+                return;
+            }
+
+            string[] pieces = req.RequestURI.Substring(ServiceURI.Length).Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            if (pieces == null || pieces.Length == 0)
+            {
+                req.WriteNotFoundResponse("404: Path error.");
+                return;
+            }
+            Dir422 dir = m_fs.GetRoot();
+            for (int i = 0; i < pieces.Length - 1; i++)
+            {
+                string piece = pieces[i];
+                dir = dir.GetDir(piece);
+                if (dir == null)
+                {
+                    req.WriteNotFoundResponse("404: Path error.");
+                    return;
+                }
+            }
+
+            string name = PercentDecoding(pieces[pieces.Length - 1]);
+
+            File422 file = dir.GetFile(name);
+            if (file != null)
+            {
+                RespondWithFile(file, req);
+                return;
+            }
+
+            dir = dir.GetDir(name);
+            if (dir == null)
+            {
+                req.WriteNotFoundResponse("404: Path error.");
+                return;
+            }
+            RespondWithList(dir, req);
+        }
+
+        private void RespondWithList(Dir422 dir, WebRequest req)
+        {
+            string html = BuildDirHTML(dir);
+            req.WriteHTMLResponse(html);
+        }
+
+        private string BuildDirHTML(Dir422 directory)
+        {
+            StringBuilder html = new StringBuilder("");
+
+            // We'll need a bit of script if uploading is allowed
+            if (m_allowUploads)
+            {
+                html.AppendLine(
+                @"<script>
+                function selectedFileChanged(fileInput, urlPrefix) 
+                {
+                    document.getElementById('uploadHdr').innerText = 'Uploading ' + fileInput.files[0].name + '...';
+                    
+                    // Need XMLHttpRequest to do the upload 
+                    if (!window.XMLHttpRequest) {
+                        alert('Your browser does not support XMLHttpRequest. Please update your browser.');
+                        return; 
+                    }
+                    
+                    // Hide the file selection controls while we upload
+                    var uploadControl = document.getElementById('uploader'); 
+                    if (uploadControl) {
+                        uploadControl.style.visibility = 'hidden'; 
+                    }
+                
+                    // Build a URL for the request
+                    if (urlPrefix.lastIndexOf('/') != urlPrefix.length - 1) {
+                    urlPrefix += '/'; 
+                    }
+                
+                    var uploadURL = urlPrefix + fileInput.files[0].name;
+                    
+                    // Create the service request object 
+                    var req = new XMLHttpRequest(); 
+                    req.open('PUT', uploadURL); 
+                    req.onreadystatechange = function() {
+                        document.getElementById('uploadHdr').innerText = 'Upload (request status == ' + req.status + ')'; 
+                    };
+                    req.send(fileInput.files[0]); 
+                }
+                </script> "
+                );
+            }
+
+            //Files
+            String dirPath = "";
+            Dir422 temp = directory;
+            while (temp.Parent != null)
+            {
+                dirPath = temp.Name + "/" + dirPath;
+                temp = temp.Parent;
+            }
+            dirPath = ServiceURI + "/" + dirPath;
+
+            if (directory.GetFiles().Count > 0)
+                html.AppendLine("<h1 style=\"color:black;\">Files</h1>");
+            foreach (File422 file in directory.GetFiles())
+            {
+                //string name = PercentEncoding(file.Name);
+                //string href = dirPath + name;
+                string href = dirPath + file.Name;
+                html.AppendFormat("<a href=\"{0}\">{1}</a>   <br>", href, file.Name);
+            }
+
+            //Directories
+            //General Note: Don't forget percent encoding and decoding.
+            if (directory.GetDirs().Count > 0)
+                html.AppendLine("<hr></hr><h1 style=\"color:black;\">Folders</h1>");
+            foreach (Dir422 d in directory.GetDirs())
+            {
+                //string name = PercentEncoding(d.Name);
+                //string href = dirPath + name;
+                string href = dirPath + d.Name;
+                html.AppendFormat("<a href=\"{0}\">{1}</a>   <br>", href, d.Name);
+            }
+
+            // If uploading is allowed, put the uploader at the bottom
+            if (m_allowUploads)
+            {
+                html.AppendFormat(
+                "<hr><h3 id='uploadHdr'>Upload</h3><br>" +
+                "<input id=\"uploader\" type='file' " + "onchange='selectedFileChanged(this,\"{0}\")' /><hr>",
+                    GetHREF(directory, true));
+            }
+            html.Append("</html>");
+
+            return html.ToString();
+        }
+
+        private string GetHREF(Dir422 dir, bool foo)
+        {
+            StringBuilder sb = new StringBuilder("/files");
+            List<string> dirs = new List<string>();
+
+            while (dir.Parent != null)
+            {
+                dirs.Add(dir.Name);
+                dir = dir.Parent;
+            }
+
+            dirs.Reverse();
+            foreach (var d in dirs)
+            {
+                sb.Append("/" + d);
+            }
+
+            return sb.ToString();
+        }
+
+
+
+        private void RespondWithFile(File422 file, WebRequest req)
+        {
+            Stream fs = file.OpenReadOnly();
+
+            if (req.Headers.ContainsKey("range"))
+            {
+                WriteWithRangeHeader(file, fs, req);
+                return;
+            }
+
+            // Send response code and headers
+            string response = "HTTP/1.1 200 OK\r\n" +
+                "Content-Length: " + fs.Length + "\r\n" +
+                "Content-Type: " + file.GetContentType() + "\r\n\r\n";
+            byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+            req.WriteResponse(responseBytes, 0, responseBytes.Length);
+                
+            while (true)
+            {
+                byte[] buf = new byte[4096];
+                int read = fs.Read(buf, 0, buf.Length);
+                if (read == 0) { break; }
+                req.WriteResponse(buf, 0, read);
+            }
+            fs.Close();
+        }
+
+        /// <summary>
+        /// Writes the with range header.
+        /// </summary>
+        private void WriteWithRangeHeader(File422 file, Stream fs, WebRequest req)
+        {
+            String[] ranges = req.Headers["range"].Trim().Substring("byte= ".Length).Split('-');
+
+            long from;
+            Int64.TryParse(ranges[0], out from);
+            if (fs.Length <= from)
+            {
+                string invalid = "HTTP/1.1 416 Requested Range Not Satisfiable\r\n\r\n";
+                byte[] invalidResponseBytes = Encoding.ASCII.GetBytes(invalid);
+                req.WriteResponse(invalidResponseBytes, 0, invalidResponseBytes.Length);
+            }
+
+            long to;
+            if (ranges[1] != string.Empty)
+                Int64.TryParse(ranges[1], out to);
+            else
+                to = fs.Length - 1;
+
+            // Send response code and headers
+            string response = "HTTP/1.1 206 Partial content\r\n" +
+                "Content-Range: bytes " + from.ToString() + "-" + to.ToString() + "/" + fs.Length.ToString() + "\r\n" +
+                "Content-Length: " + (to + 1 - from).ToString() + "\r\n" +
+                "Content-Type: " + file.GetContentType() + "\r\n\r\n";
+            byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+            req.WriteResponse(responseBytes, 0, responseBytes.Length);
+
+
+            fs.Seek(from, SeekOrigin.Begin);
+            while (true)
+            {
+                byte[] buf = new byte[4096];
+                int read = 0;
+                read = fs.Read(buf, 0, buf.Length);
+                if (read == 0) { break; }
+                req.WriteResponse(buf, 0, read);
+                if (fs.Position >= to) { break; }
+            }
+            fs.Close();
+        }
+
+        private string PercentDecoding(string name)
+        {
+            string s = name;
+
+            if (s.Contains("%20")) s = s.Replace("%20", " ");
+            if (s.Contains("%22")) s = s.Replace("%22", "\"");
+            if (s.Contains("%25")) s = s.Replace("%25", "%");
+            if (s.Contains("%2D")) s = s.Replace("%2D", "-");
+            if (s.Contains("%2E")) s = s.Replace("%2E", ".");
+            if (s.Contains("%3C")) s = s.Replace("%3C", "<");
+            if (s.Contains("%3E")) s = s.Replace("%3E", ">");
+            if (s.Contains("%5C")) s = s.Replace("%5C", "\\");
+            if (s.Contains("%5E")) s = s.Replace("%5E", "^");
+            if (s.Contains("%5F")) s = s.Replace("%5F", "_");
+            if (s.Contains("%60")) s = s.Replace("%60", "`");
+            if (s.Contains("%7B")) s = s.Replace("%7B", "{");
+            if (s.Contains("%7C")) s = s.Replace("%7C", "|");
+            if (s.Contains("%7D")) s = s.Replace("%7D", "}");
+            if (s.Contains("%7E")) s = s.Replace("%7E", "~");
+
+            return s;
+        }
+
+        /*private string PercentEncoding(string name)
+        {
+            string s = name;
+
+            if (s.Contains(" ")) s = s.Replace(" ", "%20");
+            if (s.Contains("\"")) s = s.Replace("\"", "%22");
+            //if (s.Contains("%")) s = s.Replace("%", "%25");
+            if (s.Contains("-")) s = s.Replace("-", "%2D");
+            if (s.Contains(".")) s = s.Replace(".", "%2E");
+            if (s.Contains("<")) s = s.Replace("<", "%3C");
+            if (s.Contains(">")) s = s.Replace(">", "%3E");
+            if (s.Contains("\\")) s = s.Replace("\\", "%5C");
+            if (s.Contains("^")) s = s.Replace("^", "%5E");
+            if (s.Contains("_")) s = s.Replace("_", "%5F");
+            if (s.Contains("`")) s = s.Replace("`", "%60");
+            if (s.Contains("{")) s = s.Replace("{", "%7B");
+            if (s.Contains("|")) s = s.Replace("|", "%7C");
+            if (s.Contains("}")) s = s.Replace("}", "%7D");
+            if (s.Contains("~")) s = s.Replace("~", "%7E");
+
+            return s;
+        }*/
+    }
+}
